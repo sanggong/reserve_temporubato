c7086f8c336fef1376677679766f199bf8c99ab0
sanggong <gndlmbs@naver.com>
2025-01-16 22:19:29 +0900

    url 변경

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from send_discord import discord_send_message
 
 def main():
     header = {"User-gent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0"}
-    response = requests.get("http://decoder.kr/book-rubato/", headers=header)
+    response = requests.get("http://decoder.kr/book-rubato/?ckattempt=1", headers=header)
     print(response.text)
 
     if response.status_code == 200:
